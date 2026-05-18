b1d009bf7e0463c893dba75cd4e2c29428657284
AleksandrKropachev <aleksandrkropacev777@gmail.com>
2026-05-18 23:59:23 +0500

    LAB-10

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        InputOutput io = new InputOutput("test.pas");
+
+        string error = string.Empty;
+
+        while (!io.EOF)
+        {
+            Console.WriteLine(io.CurrentChar);
+
+            io.NextCh();
+        }
+
+        Console.WriteLine("Конец файла");
+
+        foreach (string item in io.Errors)
+        {
+            error = item;
+
+            Console.WriteLine(error);
+        }
+    }
+}
